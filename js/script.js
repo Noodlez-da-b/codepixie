d799a4bbe321f8e9c6cd3ad1d62679144200381b
c-pixy <tal.sud@protonmail.com>
2024-12-16 09:24:07 +0100

    Added footer

--- js/script.js
+++ js/script.js
@@ -9,11 +9,31 @@ dDay.innerHTML = remainingDays
 
 const siteName = 'CodePixy'
 
-const letters = siteName.split('');
+const letters = siteName.split('')
 
 letters.forEach((letter, index) => {
   const letterElement = document.createElement('span')
   letterElement.textContent = letter
   siteNameElement.appendChild(letterElement)
   letterElement.style.animationDelay = `${index * 300}ms`
+})
+
+const footerText = ' © 2024 - CodePixy.com - All rights reserved '
+const footerElement = document.querySelector('.footerText')
+
+const footerElementLetters = footerText.split('')
+
+footerElementLetters.forEach((letter, index) => {
+    const footerTextEl = document.createElement('span')
+    
+    if (letter == ' ') {
+        footerTextEl.textContent = '❄️'
+    } else {
+        footerTextEl.textContent = letter
+    }
+
+    footerTextEl.style.animation = `fallAndBounce 2s ease-out forwards`
+    footerTextEl.style.animationDelay = `${index * 100}ms`
+
+    footerElement.appendChild(footerTextEl)
 })
